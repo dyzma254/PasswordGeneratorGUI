9aae6adb663384de32df1e2b62923622c72b56e3
Nikodem Owsiuk <dyzma254@gmail.com>
2021-12-14 10:41:12 +0100

    Register Form work in progress...

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -15,6 +15,7 @@ public class PasswordGenerator extends JFrame{
     private JCheckBox wielkie_check;
     private JCheckBox cyfry_check;
     private JTextField ile_cyfr;
+    private JLabel MinLabel;
 
 
     private int ile;
